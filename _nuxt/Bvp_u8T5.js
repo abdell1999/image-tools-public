85e95816b02244d25d4238b723889837d08bac45
abdell1999 <abdellahbukadduh99@gmail.com>
2026-08-01 16:00:15 +0000

    Update from https://github.com/abdell1999/image-tools/commit/a7dd3369d15bce52715a63812efe3c4908a99eb8

---
+++ _nuxt/Bvp_u8T5.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./Dg83P00k.js";import{_ as a}from"./-iTm-VWT.js";import"./DNdnNYOR.js";import"./BwoTKQS3.js";import"./DE_Y9yL9.js";import"./DxhVvjKg.js";import"./DPQ4wyIW.js";import"./BMfzxwMj.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
